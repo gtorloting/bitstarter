c1b7fb05d5e362ec7cb3c3d63bc1188f55e55173
Grégory Torloting <gregory.torloting@laposte.net>
2013-07-09 18:49:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buffer = new Buffer(255);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileName = "index.html";
+  
+  fs.exists(fileName, function(exists) {
+    if (exists) {
+      fs.stat(fileName, function(error, stats) {
+        fs.open(fileName, "r", function(error, fd) {
+          var buffer = new Buffer(stats.size);
+          fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+            var data = buffer.toString("utf8", 0, buffer.length);
+			response.send(data);
+            fs.close(fd);
+          });
+        });
+      });
+    }
+	else {
+	  response.send('The file doesn\'t exist');
+	}
+});
+  
 });
 
 var port = process.env.PORT || 5000;
